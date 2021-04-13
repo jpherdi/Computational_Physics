70678a02c88fa050573e6346910dadae5438751d
jan.herdieckerhoff@tu-dortmund.de <lewis206@Thinkpad-Lewis.localdomain>
2021-04-13 14:01:46 +0200

    :wrench: Template correction

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,8 +1,17 @@
 #include "HelloWorld.hpp"
-
-int main() {
- HelloWorld world;
- world.say();
- return 0;
+#include <Eigen/Dense>
+ 
+using Eigen::MatrixXd;
+ 
+int main()
+{
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+    HelloWorld world;
+    world.say();
+    return 0;
 }
-
