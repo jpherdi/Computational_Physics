7f6153453b8627d28417f62ad41c9ef51b65177e
Jan Herdieckerhoff <jherdieckerhoff@point-8.de>
2021-04-22 21:29:04 +0200

    :memo: New template

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <math.h>
+#include <fstream>
 
- 
+const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, "\t", "\n");
+
 int main()
 {
-    Eigen::MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
-    std::cout << "Everything works!" << std::endl;
+	// Initialization of vectors and matrices
+    Eigen::VectorXd v(3);
+	Eigen::MatrixXd matA(3,3);
+
+	v << 0, 1, 2;
+	matA << v, v, v;
+ 
+	std::ofstream output;
+	output.open("bin/Aufgabe1.txt", std::ofstream::out | std::ofstream::trunc);
+	output << "Matrix matA:\n" << matA.format(CSVFormat) << std::endl;
+	output.close();
+
     return 0;
 }
