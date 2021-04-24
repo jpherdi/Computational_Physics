d73c3fab866ec0c539cf016665ef36a088bdd752
Jan Herdieckerhoff <jherdieckerhoff@point-8.de>
2021-04-24 13:56:18 +0200

    :rocket: Finished code for Blatt2

--- Blatt2/src/script.py
+++ Blatt2/src/script.py
@@ -12,4 +12,21 @@ for i in ["",10,20,50]:
     plt.figure(figsize=(12,12))
     plt.imshow(a, cmap="gray")
     plt.tight_layout()
-    plt.savefig("bin/Bild"+str(i)+".pdf"),
+    plt.savefig("bin/Bild"+str(i)+".pdf")
+
+t_random, t_LU, t_solve = np.genfromtxt("bin/times.txt", unpack=True)
+
+num = np.arange(1,len(t_random)+1)
+
+plt.figure(figsize=(12,8))
+plt.plot(num, t_random, "x", label=r"$t_\mathrm{random}$")
+plt.plot(num, t_LU, "x", label=r"$t_\mathrm{LU}$")
+plt.plot(num, t_solve, "x", label=r"$t_\mathrm{solve}$")
+plt.yscale("log")
+plt.xscale("log")
+plt.xlabel(r"Size of Vector $N$")
+plt.ylabel(r"time $t / \mathrm{s}$")
+plt.grid()
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("bin/times.pdf")
